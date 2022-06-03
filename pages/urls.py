8d7ddf263f11750723f9ffd957fff32b73bb50da
eunjung0613 <dmswjd7281@naver.com>
2022-06-03 12:15:20 +0900

    [#1]create project frame

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.mainpage),
     path('company/',views.company),
+    path('company/history/',views.history),
+    path('company/target',views.target),
+    path('company/business_area',views.business_area),
+    path('company/information',views.information),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,15 @@ def mainpage(request):
 
 def company(request):
     return render(request, 'pages/company_info.html')
+
+def history(request):
+    return render(request, 'pages/history.html')
+
+def target(request):
+    return render(request, 'pages/target.html')
+
+def business_area(request):
+    return render(request, 'pages/business_area.html')
+
+def information(request):
+    return render(request, 'pages/information.html')
